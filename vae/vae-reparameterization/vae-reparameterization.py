804b3face79f14abb467d103587b163b5e050e95
HQN <146404673+hoangquangninh@users.noreply.github.com>
2026-03-21 00:14:10 +0900

    Update solution for vae-reparameterization

--- vae/vae-reparameterization/vae-reparameterization.py
+++ vae/vae-reparameterization/vae-reparameterization.py
@@ -4,11 +4,9 @@ def reparameterize(mu: np.ndarray, log_var: np.ndarray) -> np.ndarray:
     """
     Sample from latent distribution using reparameterization trick.
     """
-    n_rows, n_cols = mu.shape
     # Your implementation here
+    n_rows, n_cols = mu.shape
     rng = np.random.default_rng()
-    
-    # Generate a single random number from N(0, 1)
     epsilon = rng.standard_normal(size=(n_rows, n_cols))
 
     return mu + np.exp(log_var)*epsilon
